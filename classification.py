64d12155e80ea66a68837177e7ab1b3a683555e8
vidarli <vidar@vlpn.net>
2011-11-22 16:25:33 +0100

    Cross-validation implemented

--- classification.py
+++ classification.py
@@ -1,16 +1,51 @@
 import sys
 import orange, orngTest
+import random
 
 data = orange.ExampleTable("features.tab")
-l = orange.SVMLearner(data)
+l = orange.BayesLearner(data)
 #l.nu = 0.1
 #l.svm_type = orange.SVMLearner.Nu_SVC
 
+def split(data,n):
+    i = 0
+    while i < len(data):
+        yield data[i:i+n]
+        i += n
 
-print 'ranked: no'
+def cross_validation():
+    k = 10
+    d = list(data)
+    nps = len(d) / 10
+    acs = []
+    for i in range(k):
+        random.shuffle(d)
+        subsamples = list(split(d,nps))
+        accurancy = 0
+        for j in range(k):
+            validation = subsamples[j]
+            training = []
+            for s in subsamples:
+                if s == validation: continue
+                training += s
+            l = orange.BayesLearner(training)
+            thisa = 0
+            for ex in validation:
+                if ex.getclass() == l(ex):
+                    thisa += 1
+            accurancy += float(thisa) / len(validation)
+        acs.append(accurancy)
+    print sum(acs) / float(k) / float(k)
 
-correct = 0
-for ex in data:
-    print ex['id'], l(ex)
+if __name__ == '__main__':
+    import sys
+    if 'cross' in sys.argv:
+        cross_validation()
+    else:
+        print 'ranked: no'
+
+        correct = 0
+        for ex in data:
+            print ex['id'], l(ex)
     
 
